6dfa17bef8a480a5a1c21885dafedd355617558a
Humberto Dominique <hdominique240@gmail.com>
2023-04-24 08:42:30 -0400

    desafío2 ProductManager.js

--- desafio2_HDominique.js
+++ desafio2_HDominique.js
@@ -3,11 +3,14 @@ const fs = require("fs");
 class ProductManager {
   #id = 0;
 
+  //Se crea variable this.path que recibe desde el constructor de la instancia, la ruta del archivo en el que serán almacenados los productos.
   constructor(ruta) {
     this.path = ruta;
 
     fs.writeFileSync(`${this.path}`, JSON.stringify([]));
   }
+
+  //Método addProducts que agrega productos al arreglo.
 
   async addProduct(title, description, price, thumbnail, code, stock) {
     try {
@@ -47,7 +50,7 @@ class ProductManager {
         JSON.stringify(productosGuardados)
       );
     } catch (err) {
-      console.log("Error durante la lectura inicial de productos");
+      console.log("\nError durante la lectura inicial de productos");
     }
   }
 
@@ -56,6 +59,7 @@ class ProductManager {
     return this.#id;
   }
 
+  //Método getProductos que trae todos los productos presentes en el arreglo.
   async getProducts() {
     try {
       const productosGuardados = await fs.promises.readFile(
@@ -64,9 +68,11 @@ class ProductManager {
       );
       return JSON.parse(productosGuardados);
     } catch (err) {
-      console.log("Error durante la revisión de los productos");
+      console.log("\nError durante la revisión de los productos");
     }
   }
+
+  //Método getProductsById que trae el producto con el id seleccionado o indica error en caso de no existir producto con ese id.
 
   async getProductsById(idProducto) {
     try {
@@ -78,16 +84,18 @@ class ProductManager {
 
       if (productsIndex === -1) {
         ("/n");
-        console.log("El producto que está tratando de buscar no existe.");
+        console.log("\nEl producto que está tratando de buscar no existe.");
       } else {
         ("/n");
-        console.log("El producto que está buscando por id es:");
+        console.log("\nEl producto que está buscando por id es:");
         console.log(productosGuardados[productsIndex]);
       }
     } catch (err) {
-      console.log("Error al buscar producto por su id");
+      console.log("\nError al buscar producto por su id");
     }
   }
+
+  //Método updateProduct que permite modificar las propiedades de algún producto seleccionado por su id sin modificar su id.
 
   async updateProduct(idProducto, modificacion) {
     try {
@@ -98,7 +106,7 @@ class ProductManager {
       );
 
       if (productsIndex === -1) {
-        console.log("El producto que está tratando de modificar no existe.");
+        console.log("\nEl producto que está tratando de modificar no existe.");
       } else {
         let objetoModificacion = modificacion;
 
@@ -117,12 +125,14 @@ class ProductManager {
           JSON.stringify(productosGuardados)
         );
 
-        console.log("El producto modificado es el siguiente: \n", update);
+        console.log("\nEl producto modificado es el siguiente: \n", update);
       }
     } catch (err) {
-      console.log("Error al modificar el producto");
+      console.log("\nError al modificar el producto");
     }
   }
+
+  //Método deleteProduct que permite eliminar un producto del arreglo seleccionado por su id e indica error en caso de no existir producto con ese id.
 
   async deleteProduct(idProducto) {
     try {
@@ -133,7 +143,7 @@ class ProductManager {
       );
 
       if (productsIndex === -1) {
-        console.log("El producto que está tratando de borrar no existe.");
+        console.log("\nEl producto que está tratando de borrar no existe.");
       } else {
         console.log(
           "\nProducto eliminado con el siguiente id:",
@@ -141,8 +151,7 @@ class ProductManager {
         );
         console.log(
           "Nombre del producto: ",
-          productosGuardados[productsIndex].title,
-          "\n"
+          productosGuardados[productsIndex].title
         );
 
         productosGuardados.splice(productsIndex, 1);
@@ -153,67 +162,74 @@ class ProductManager {
         );
       }
     } catch (err) {
-      console.log("Error al buscar producto por su id");
+      console.log("\nError al buscar producto por su id");
     }
   }
 }
 
 //TESTING DEL DESAFÍO.
 
-const productManager = new ProductManager("./check.json");
+//Se genera la instancia y se especifica la ruta de almacenamiento de los productos.
+const productManager = new ProductManager("./test.json");
 
 const test = async () => {
   try {
-    console.log(await productManager.getProducts());
+    //Se evalúa que la instancia inicialmente sea un arreglo vacío.
+
+    console.log("Arreglo vacío: ", await productManager.getProducts());
+
+    //Se evalúa que se cree producto con id autogenerado. Se mantienen validaciones en caso de ingresar códigos duplicados y que todos los campos sean obligatorios.
 
     await productManager.addProduct(
-      "ProductoB",
-      "DEF",
-      "2000",
-      "Con Imagen",
+      "producto prueba 1",
+      "Este es un producto prueba",
+      200,
+      "Sin imagen",
       "abc123",
-      "20"
+      25
     );
 
     await productManager.addProduct(
-      "ProductoC",
-      "DEF",
-      "2000",
-      "Con Imagen",
+      "producto prueba 2",
+      "Este es un producto prueba",
+      200,
+      "Sin imagen",
       "abc124",
-      "20"
+      25
     );
 
     await productManager.addProduct(
-      "ProductoD",
-      "DEF",
-      "2000",
-      "Con Imagen",
+      "producto prueba 3",
+      "Este es un producto prueba",
+      200,
+      "Sin imagen",
       "abc125",
-      "20"
+      25
     );
 
-    console.log(await productManager.getProducts());
+    //Se evalúa que método getProducts traiga todos los productos agregados.
+    console.log("\n", await productManager.getProducts());
 
+    //Se evalúa que método getProductsById arroje el producto con el id seleccionado o indique error en caso de no existir producto con ese id.
     await productManager.getProductsById(3);
 
-    await productManager.updateProduct(2, {
-      title: "PROBANDOO",
-      description: "9999",
-      price: 9999,
-      id: 66,
-    });
-
+    //Se evalúa que método updateProducts modifique las propiedades de los productos seleccionados por su id o indique error en caso de no existir producto con ese id.
     await productManager.updateProduct(1, {
       title: "SE MODIFICÓ",
       description: "SE MODIFICÓ",
       price: "SE MODIFICÓ",
-      id: 66,
     });
 
+    await productManager.updateProduct(2, {
+      thumbnail: "SE MODIFICÓ",
+      stock: "SE MODIFICÓ",
+      code: "SE MODIFICÓ",
+    });
+
+    //Se evalúa que método deleteProduct elimine del arreglo el producto seleccionado por su id o indique error en caso de no existir producto con ese id.
     await productManager.deleteProduct(1);
   } catch (err) {
-    console.log("Salio mal el Test");
+    console.log("Error durante la ejecución del Test");
   }
 };
 
